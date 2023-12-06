bd59776bf9dd94deb24bdebe184329d552d3baa2
gjkt2001 <2910223554@qq.com>
2023-12-06 15:18:20 +0800

    西宁野生动物园-前端-后台

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      Xyz66博客管理系统
+    </div>
+    <div>
+      欢迎关注Xyz的B站账号：<a href="https://space.bilibili.com/8443394">https://space.bilibili.com/8443394</a>  猫三棂
+    </div>
+    <div>
+      Hello,World。
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
