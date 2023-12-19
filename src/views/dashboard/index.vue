4e3132c77f7b55dd1266e0c971d8afabb4652af9
gjkt <2910223554@qq.com>
2023-12-19 11:22:53 +0800

    禁用ESlint-Vue格式检查

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,7 +2,7 @@
   <div class="dashboard-container">
     <div class="dashboard-text">Xyz66西宁野生动物园论坛后台</div>
     <div>
-      欢迎关注Xyz的B站账号：<a href="https://space.bilibili.com/8443394"
+      欢迎关注Xyz的B站账号:<a href="https://space.bilibili.com/8443394"
         >https://space.bilibili.com/8443394</a
       >
       猫三棂
